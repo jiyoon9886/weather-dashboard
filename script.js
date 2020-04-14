d03dc746596405e884fccd51287296b02a113183
Jiyoon Kim <jiyoon9886@gmail.com>
2020-04-13 22:09:20 -0400

    created html, css, and script files

---
+++ script.js
@@ -0,0 +1,8 @@
+var userCity = $("#city-searched");
+console.log(userCity);
+// When the user inputs a city name in the
+// I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
+var apiKey = "4a8970f1ceb21011e299d68d17d14399";
+
+//var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + {city name} + "&appid=" + APIKey;
